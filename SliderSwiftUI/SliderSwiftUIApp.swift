af989f8dc89c62a69e100af46a8cedef7acabb65
Bellaaspera <lanasenatorova@gmail.com>
2022-09-20 19:40:05 +0300

    Initial Commit

---
+++ SliderSwiftUI/SliderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderSwiftUIApp.swift
+//  SliderSwiftUI
+//
+//  Created by Светлана Сенаторова on 20.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
